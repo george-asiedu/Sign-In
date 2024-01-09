a3aab24f3657c2efe4ca989c18417808eb4ef8ca
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-09 16:02:20 +0100

    update

--- sign-in/src/app/app.component.ts
+++ sign-in/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
@@ -18,4 +18,14 @@ export class AppComponent {
 
   faEnvelope = faEnvelope
   faLock = faLock
+
+  applyForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  })
+
+  submitForm() {
+    this.applyForm.value.email ?? ''
+    this.applyForm.value.password ?? ''
+  }
 }
